06b661253ac74306a5164e967abff3da07d7ac00
ayumitamai97 <ayumitamai97@gmail.com>
2018-10-18 15:51:17 +0900

    Modified port number

--- server.py
+++ server.py
@@ -24,5 +24,5 @@ application = tornado.web.Application([
 
 if __name__ == "__main__":
     wsgi_app = tornado.wsgi.WSGIAdapter(application)
-    server = wsgiref.simple_server.make_server('', 8888, wsgi_app)
+    server = wsgiref.simple_server.make_server('', 8080, wsgi_app)
     server.serve_forever()
