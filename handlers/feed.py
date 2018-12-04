e1d6ff8e818ff65a56e22b4f4d0653738cc43674
ayumitamai97 <ayumitamai97@gmail.com>
2018-12-05 00:09:56 +0900

    Show feed in /

--- handlers/feed.py
+++ handlers/feed.py
@@ -8,7 +8,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (Column, String, Text, ForeignKey, \
                 create_engine, MetaData, DECIMAL, DATETIME, exc, event, Index)
 import os
-import pdb
+import re
+import pdb # python debugger
 
 class FeedHandler(ViewHandler):
   def initialize(self, **config):
@@ -28,19 +29,33 @@ class FeedHandler(ViewHandler):
     Session = sessionmaker(bind=ENGINE)
     session = Session()
 
-    musicians = session.query(Musician).all()
+    musicians_query = session.query(Musician)
+    musicians = musicians_query.all()
     musician_ids = list(map(lambda m: m.id, musicians))
 
     all_live_info = {}
 
     for musician_id in musician_ids:
+      musician_name = musicians_query.filter(Musician.id == musician_id).first().name
       live_info_list = session \
                         .query(UpdatedLiveInfo) \
                         .filter(UpdatedLiveInfo.musician_id == musician_id) \
                         .order_by(UpdatedLiveInfo.created_at.desc()) \
                         .all()
+
+      pdb.set_trace()
       for live_info in live_info_list:
-        created_at = live_info.created_at
-        all_live_info[created_at] = { musician_id: live_info.content }
+        created_at = str(live_info.created_at)
+        jscontent = re.findall(r'(.*\(.*\);.*|.*\{.*\}.*|.+=.+;|\n{2,}|<[a-z]+>)', live_info.content)
+        content = live_info.content
+
+        if not jscontent == []:
+          for jscont in jscontent:
+            content = content.replace(jscont, "")
+
+      if created_at in all_live_info:
+        all_live_info[created_at].append({ musician_name: content })
+      else:
+        all_live_info[created_at] = [{ musician_name: content }]
 
     return self.render("../templates/feed.html", live_info=all_live_info)
